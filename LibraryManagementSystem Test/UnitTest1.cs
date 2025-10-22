fcd695af6ca32b0215536606ab4308d9e408b42a
Thandokuhle Dlamini <ST10447313@vcconnect.edu.za>
2025-10-22 14:11:09 +0200

    Add more to the uint testing

--- LibraryManagementSystem Test/UnitTest1.cs
+++ LibraryManagementSystem Test/UnitTest1.cs
@@ -2,6 +2,7 @@ using Xunit;
 using LibraryManagementSystem.Services;
 using LibraryManagementSystem.Data;
 using LibraryManagementSystem.Models;
+using System.Text;
 
 namespace LibraryManagementSystem_Test
 {
@@ -37,6 +38,39 @@ namespace LibraryManagementSystem_Test
             //Verify if we can retrieve the book
             var retrievedBook = BookDataStore.GetBookById(newBook.Id);
 
+        }
+
+        [Fact]
+        public async Task Test2_EncyptionFile_Successful()
+        {
+            var originalContent = "this is a secret file content thar should be encrypted";
+            var originalBytes = Encoding.UTF8.GetBytes(originalContent);
+            var inputStream = new MemoryStream(originalBytes);
+            var tempFile = Path.GetTempFileName();
+            var encryptionServices = new FileEncryptionService();
+
+            try
+            {
+                await encryptionServices.EncryptFileAsync(inputStream, tempFile);
+                Assert.True(File.Exists(tempFile), "Encrypted file should exist");
+
+                //Read the encryption file
+                var encryptedBytes = await File.ReadAllBytesAsync(tempFile);
+
+                //Assert that the encrypted data is not the same as the original
+                Assert.NotEqual(originalBytes, encryptedBytes);
+
+                //Verify the encrypted
+                Assert.True(encryptedBytes.Length > 0, "Encrypted file should have content");
+
+                //Verify an account read the original text from the encryption file
+                var encryptedText = Encoding.UTF8.GetString(encryptedBytes);
+                Assert.DoesNotContain("this is a secret file content thar should be encrypted", encryptedText);
+            }
+            finally
+            {
+                if (File.Exists(tempFile)) File.Delete(tempFile);    
+            }
         }
     }
 }
